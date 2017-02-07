40ca10775a99c0f3502fc9a6c535c0f705a7593b
Dan Padgett <dumbledore3@gmail.com>
2017-02-07 08:12:27 +0000

    correct quoting of LCD command in sylar_sync

--- scripts/sylar_sync.sh
+++ scripts/sylar_sync.sh
@@ -8,5 +8,5 @@ CD="/"
 #lftp -c "set ftp:timezone CET; set ftp:passive-mode off; set ftp:port-range 65000-65100; set net:timeout 5; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd $LCD; cd $CD && mirror --verbose -P 8"
 
 # more efficient to rsync on remote file host, since it can more efficiently checksum existing files
-ssh -A ${NAS_USER}@${NAS_HOST} -p ${NAS_PORT} "lftp -c \"set ftp:timezone CET; set ftp:passive-mode off; set ftp:port-range 65000-65100; set net:timeout 5; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd $LCD; cd $CD && mirror --verbose -P 8\"" \
+ssh -A ${NAS_USER}@${NAS_HOST} -p ${NAS_PORT} "lftp -c \"set ftp:timezone CET; set ftp:passive-mode off; set ftp:port-range 65000-65100; set net:timeout 5; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd '$LCD'; cd $CD && mirror --verbose -P 8\"" \
  | tee -a /dev/stderr | grep "^Transferring file" | sed "s/Transferring file .\(.*\).$/\1/" | grep ".dm_2[5-6]$" | sed 's|^|/cygdrive/U/demos/sylar/|' >> /tmp/demos.txt
