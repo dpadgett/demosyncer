764edf47e600d2a9bba5b5298763f574ba196988
Dan Padgett <dumbledore3@gmail.com>
2017-02-07 07:58:00 +0000

    add quotes for rsync scripts

--- scripts/refresh_sync.sh
+++ scripts/refresh_sync.sh
@@ -3,5 +3,5 @@
 #rsync -avzL --append-verify teh@pug.jactf.com:/home/teh/demos/ /cygdrive/U/demos
 
 # more efficient to rsync on remote file host, since it can more efficiently checksum existing files
-ssh -A ${NAS_USER}@${NAS_HOST} -p ${NAS_PORT} "rsync -avzL --append-verify teh@pug.jactf.com:/home/teh/demos/ ${NAS_DIR}" \
+ssh -A ${NAS_USER}@${NAS_HOST} -p ${NAS_PORT} "rsync -avzL --append-verify teh@pug.jactf.com:/home/teh/demos/ \"${NAS_DIR}\"" \
  | tee -a /dev/stderr | grep ".dm_2[5-6]$" | sed 's|^|/cygdrive/U/demos/|' >> /tmp/demos.txt
