15eccaa7495ba7bce79d89b1b819a7082d00b27b
Dan Padgett <dumbledore3@gmail.com>
2017-02-07 07:16:30 +0000

    add env vars to customize NAS server address

--- scripts/bra_sync.sh
+++ scripts/bra_sync.sh
@@ -3,11 +3,11 @@ HOST="bra.jactf.com:21"
 USER=${BRA_USER}
 PASS=${BRA_PASS}
 FTPURL="ftp://$USER:$PASS@$HOST"
-LCD="/cygdrive/D/demos/bra"
+LCD="${NAS_DIR}/bra"
 CD="/autorecord"
 #lftp -c "set ftp:timezone CET; set ftp:passive-mode off; set ftp:port-range 65000-65100; set net:timeout 5; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd '$LCD'; cd '$CD' && mirror --verbose"
 #lftp -c "set ftp:timezone UTC; set net:timeout 15; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd '$LCD'; cd '$CD' && mirror --verbose -P 8"
 
 # more efficient to rsync on remote file host, since it can more efficiently checksum existing files
-ssh -A dan@dpadgett.org "lftp -c \"set ftp:timezone UTC; set net:timeout 15; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd '$LCD'; cd '$CD' && mirror --verbose -P 8\"" \
+ssh -A ${NAS_USER}@${NAS_HOST} -p ${NAS_PORT} "lftp -c \"set ftp:timezone UTC; set net:timeout 15; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd '$LCD'; cd '$CD' && mirror --verbose -P 8\"" \
  | tee -a /dev/stderr | grep "^Transferring file" | sed "s/Transferring file .\(.*\).$/\1/" | grep ".dm_2[5-6]$" | sed 's|^|/cygdrive/U/demos/bra/|' >> /tmp/demos.txt
--- scripts/sylar_sync.sh
+++ scripts/sylar_sync.sh
@@ -3,10 +3,10 @@ HOST="sylar.jactf.com:21"
 USER=${SYLAR_USER}
 PASS=${SYLAR_PASS}
 FTPURL="ftp://$USER:$PASS@$HOST"
-LCD="/cygdrive/D/demos/sylar"
+LCD="${NAS_DIR}/sylar"
 CD="/"
 #lftp -c "set ftp:timezone CET; set ftp:passive-mode off; set ftp:port-range 65000-65100; set net:timeout 5; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd $LCD; cd $CD && mirror --verbose -P 8"
 
 # more efficient to rsync on remote file host, since it can more efficiently checksum existing files
-ssh -A dan@dpadgett.org "lftp -c \"set ftp:timezone CET; set ftp:passive-mode off; set ftp:port-range 65000-65100; set net:timeout 5; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd $LCD; cd $CD && mirror --verbose -P 8\"" \
+ssh -A ${NAS_USER}@${NAS_HOST} -p ${NAS_PORT} "lftp -c \"set ftp:timezone CET; set ftp:passive-mode off; set ftp:port-range 65000-65100; set net:timeout 5; set net:max-retries 2; set net:reconnect-interval-base 5; open '$FTPURL'; lcd $LCD; cd $CD && mirror --verbose -P 8\"" \
  | tee -a /dev/stderr | grep "^Transferring file" | sed "s/Transferring file .\(.*\).$/\1/" | grep ".dm_2[5-6]$" | sed 's|^|/cygdrive/U/demos/sylar/|' >> /tmp/demos.txt
